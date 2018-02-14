9865e5bb839c527466f429e434289b32ab418edc
Jamie Howard <jamiehowardco@gmail.com>
2018-02-14 14:37:13 -0500

    Changed transaction precision

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\Models\Transaction::class, function (Faker $faker) {
     return [
         'source_id' => rand(1,10000),
         'destination_id' => rand(1,10000),
-        'amount' => mt_rand(1,10)+number_format(mt_rand(1,100)*.100, 2),
+        'amount' => mt_rand(1,10)+number_format(mt_rand(1,100)*.1000, 2),
     ];
 });
